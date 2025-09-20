1815801cc8fc5272d4f7e94a27ce1bdf8821eef4
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2025-09-20 18:10:03 +0700

    add ESLint directive to disable no-explicit-any rule in ScrollReveal component

--- components/ScrollReveal.tsx
+++ components/ScrollReveal.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import React, { useEffect, useRef, useMemo, ReactNode, RefObject } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
